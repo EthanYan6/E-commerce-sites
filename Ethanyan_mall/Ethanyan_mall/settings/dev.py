9c2deb22fa091938c5b599ba9375eda49cf75ec1
Ethanyan <yanyuliang6@163.com>
2019-01-22 15:38:16 +0800

    语言时区本地化

--- Ethanyan_mall/Ethanyan_mall/settings/dev.py
+++ Ethanyan_mall/Ethanyan_mall/settings/dev.py
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
