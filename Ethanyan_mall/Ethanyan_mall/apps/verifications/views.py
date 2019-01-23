1045304c075af2bc6eb190679feed69052ff4738
Ethanyan <yanyuliang6@163.com>
2019-01-23 11:20:41 +0800

    短信验证码业务逻辑分析

--- Ethanyan_mall/Ethanyan_mall/apps/verifications/views.py
+++ Ethanyan_mall/Ethanyan_mall/apps/verifications/views.py
@@ -6,4 +6,15 @@ from rest_framework.views import APIView
 # GET /sms_codes/(?P<mobile>1[3-9]\d{9})/
 class SMSCodeView(APIView):
     """短信验证码"""
-    pass
+    def get(self,request,mobile):
+        """
+        获取短信验证码
+        1.随机生成6位的数字作为短信验证码
+        2.在redis中存储短信验证码内容，以'sms_<mobile>'为key，以验证码的内容为value
+        3.使用云通讯给mobile发送短信
+        4.返回应答，短信发送成功
+        """
+        # 1.随机生成6位的数字作为短信验证码
+        # 2.在redis中存储短信验证码内容，以'sms_<mobile>'为key，以验证码的内容为value
+        # 3.使用云通讯给mobile发送短信
+        # 4.返回应答，短信发送成功
