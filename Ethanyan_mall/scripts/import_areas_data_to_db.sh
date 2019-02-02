c8fd62508269a8711e47c6e76e0e39c9a2696cb4
EthanYan6 <ethanyanisme@gmail.com>
2019-02-02 11:43:43 +0800

    商品部分-添加测试数据

--- Ethanyan_mall/scripts/import_areas_data_to_db.sh
+++ Ethanyan_mall/scripts/import_areas_data_to_db.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 #mysql -h数据库ip地址 -u数据库用户名 -p 数据库密码 < areas.sql
  mysql -h127.0.0.1 -umeiduo -pmeiduo meiduo_mall < areas.sql
---
+++ Ethanyan_mall/scripts/import_goods_data_to_db.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+mysql -h172.17.0.1 -umeiduo -pmeiduo meiduo_mall < goods_data.sql
