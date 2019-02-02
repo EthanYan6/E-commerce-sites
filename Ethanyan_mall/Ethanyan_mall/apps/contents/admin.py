57a291e5db1b9112c1f9a25e7a41f0138be2241f
EthanYan6 <ethanyanisme@gmail.com>
2019-02-02 11:47:45 +0800

    商品部分-添加测试数据2

--- Ethanyan_mall/Ethanyan_mall/apps/contents/admin.py
+++ Ethanyan_mall/Ethanyan_mall/apps/contents/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from contents import models
 # Register your models here.
+
+admin.site.register(models.ContentCategory)
+admin.site.register(models.Content)
--- Ethanyan_mall/Ethanyan_mall/apps/goods/admin.py
+++ Ethanyan_mall/Ethanyan_mall/apps/goods/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from goods.models import Goods
+from goods import models
 # Register your models here.
 
-admin.site.register(Goods)
+admin.site.register(models.GoodsCategory)
+admin.site.register(models.GoodsChannel)
+admin.site.register(models.Goods)
+admin.site.register(models.Brand)
+admin.site.register(models.GoodsSpecification)
+admin.site.register(models.SpecificationOption)
+admin.site.register(models.SKU)
+admin.site.register(models.SKUSpecification)
+admin.site.register(models.SKUImage)
