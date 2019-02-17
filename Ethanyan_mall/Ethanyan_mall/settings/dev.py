9966a93ae8c396910b7ef9993b29013d66ca160b
EthanYan6 <ethanyanisme@gmail.com>
2019-02-17 15:52:14 +0800

    配置haystack

--- Ethanyan_mall/Ethanyan_mall/settings/dev.py
+++ Ethanyan_mall/Ethanyan_mall/settings/dev.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'ckeditor_uploader',  # 富文本编辑器上传图片模块
     'django_crontab',  # 定时任务
     'pics.apps.PicsConfig',
+    'haystack',
     # 以后将安装的包放在上面，应用的包放在下面
     # 'Ethanyan_mall.apps.users.apps.UsersConfig',
     'users.apps.UsersConfig',
@@ -342,3 +343,18 @@ CRONJOBS = [
 
 # 解决crontab中文问题
 CRONTAB_COMMAND_PREFIX = 'LANG_ALL=zh_cn.UTF-8'
+
+# Haystack全文检索框架配置
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        # 指定所使用的搜索引擎
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        # 指定es搜索引擎服务器地址
+        'URL': 'http://192.168.59.225:9200//',  # 此处为elasticsearch运行的服务器ip地址，端口号固定为9200
+        # 指定elasticsearch建立的索引库的名称
+        'INDEX_NAME': 'meiduo',  # 指定elasticsearch建立的索引库的名称
+    },
+}
+
+# 当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
