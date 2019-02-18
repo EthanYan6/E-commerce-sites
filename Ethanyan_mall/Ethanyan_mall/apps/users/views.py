7b2f84d19f7b89ab84fc76dc462ae2347d5e42f8
EthanYan6 <ethanyanisme@gmail.com>
2019-02-18 23:45:31 +0800

    购物车部分-购物车记录合并(合并函数调用-普通账户密码登录)

--- Ethanyan_mall/Ethanyan_mall/apps/users/views.py
+++ Ethanyan_mall/Ethanyan_mall/apps/users/views.py
@@ -15,7 +15,37 @@ from rest_framework.mixins import RetrieveModelMixin, CreateModelMixin, UpdateMo
 from users.serializers import EmailSerializer
 from goods.models import SKU
 from goods.serializers import SKUSerializer
+
+from datetime import datetime
+from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.views import ObtainJSONWebToken,jwt_response_payload_handler
+
+from cart.utils import merge_cookie_cart_to_redis
 # Create your views here.
+
+
+# POST /authorizations/
+class UserAuthorizeView(ObtainJSONWebToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.object.get('user') or request.user
+            token = serializer.object.get('token')
+            response_data = jwt_response_payload_handler(token, user, request)
+            response = Response(response_data)
+            if api_settings.JWT_AUTH_COOKIE:
+                expiration = (datetime.utcnow() +
+                              api_settings.JWT_EXPIRATION_DELTA)
+                response.set_cookie(api_settings.JWT_AUTH_COOKIE,
+                                    token,
+                                    expires=expiration,
+                                    httponly=True)
+            # 调用合并购物车记录函数
+            merge_cookie_cart_to_redis(request, user, response)
+            return response
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # POST /browse_histories/
 class HistoryView(CreateAPIView):
