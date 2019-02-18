486beb89bc84a6a1ea370dc289f38e5c3c5ba625
EthanYan6 <ethanyanisme@gmail.com>
2019-02-18 14:39:34 +0800

    购物车部分-购物车记录获取_后端API(登录用户和未登录用户)

--- Ethanyan_mall/Ethanyan_mall/apps/cart/serializers.py
+++ Ethanyan_mall/Ethanyan_mall/apps/cart/serializers.py
@@ -25,3 +25,12 @@ class CartSerializer(serializers.Serializer):
             raise serializers.ValidationError('商品库存不足')
 
         return attrs
+
+class CartSKUSerializer(serializers.ModelSerializer):
+    """购物车商品序列化器类"""
+    count = serializers.IntegerField(label='数量')
+    selected = serializers.BooleanField(label='勾选状态')
+
+    class Meta:
+        model = SKU
+        fields = ('id','name','price','default_image_url','count','selected')
--- Ethanyan_mall/Ethanyan_mall/apps/cart/views.py
+++ Ethanyan_mall/Ethanyan_mall/apps/cart/views.py
@@ -10,7 +10,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from cart import constants
-from cart.serializers import CartSerializer
+from cart.serializers import CartSerializer, CartSKUSerializer
+from goods.models import SKU
 
 
 class CartView(APIView):
@@ -93,3 +94,74 @@ class CartView(APIView):
             cart_data = base64.b64encode(pickle.dumps(cart_dict)).decode() # str
             response.set_cookie('cart',cart_data,max_age=constants.CART_COOKIE_EXPIRES)
             return response
+    # GET /cart/
+    def get(self,request):
+        """
+        购物车记录获取
+        1.获取用户的购物车记录
+            1.1如果用户已登录，从redis中获取用户的购物车记录
+            1.2如果用户未登录，从cookie中获取用户的购物车记录
+        2.根据用户购物车中商品id获取对应商品的数据
+        3.将购物车商品的数据序列化并返回
+        """
+        try:
+            user = request.user
+        except Exception:
+            user = None
+        # 1.获取用户的购物车记录
+        if user and user.is_authenticated:
+            # 1.1如果用户已登录，从redis中获取用户的购物车记录
+            # 获取redis链接
+            redis_conn = get_redis_connection('cart')
+
+            # 从redis hash中获取用户购物车中添加的商品id和对应的数量count
+            cart_key = 'cart_%s' % user.id
+            # {
+            #     b'<sku_id>': b'<count>',
+            #     ...
+            # }
+            cart_redis = redis_conn.hgetall(cart_key)
+
+            # 从redis set中获取用户购物车中勾选的商品id
+            cart_selected_key = 'cart_selected_%s' % user.id
+
+            # (b'<sku_id>',...)
+            cart_selected_redis = redis_conn.smembers(cart_selected_key)
+
+            cart_dict = {}
+            for sku_id,count in cart_redis.items():
+                cart_dict[int(sku_id)] = {
+                    'count':int(count),
+                    'selected':sku_id in cart_selected_redis
+                }
+        else:
+            # 1.2如果用户未登录，从cookie中获取用户的购物车记录
+            # 获取cookie中的购物车数据
+            cookie_cart = request.COOKIES.get('cart')
+
+            if cookie_cart:
+                # 解析cookie中购物车数据
+                # {
+                #     '<sku_id>': {
+                #         'count': '<count>',
+                #         'selected': '<selected>'
+                #     },
+                #     ...
+                # }
+                cart_dict = pickle.loads(base64.b64decode(cookie_cart))
+            else:
+                cart_dict = {}
+
+        # 2.根据用户购物车中商品id获取对应商品的数据
+        sku_ids = cart_dict.keys()
+        skus = SKU.objects.filter(id__in = sku_ids)
+
+        for sku in skus:
+            # 给sku对象增加属性count和selected，分别保存该商品在购物车中添加的数量和勾选状态
+            sku.count = cart_dict[sku.id]['count']
+            sku.selected = cart_dict[sku.id]['selected']
+
+
+        # 3.将购物车商品的数据序列化并返回
+        serializer = CartSKUSerializer(skus,many=True)
+        return Response(serializer.data)
